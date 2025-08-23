b1e72a81e032424780a2f51f42f62557afba92d7
ashamit <ashwiniamit13@gmail.com>
2025-08-23 19:43:45 +0530

    Print occurrences of Words In Given String

--- InterviewCodingPractice/src/Practice/PrintOccuranceOfWordsInGivenString.java
+++ InterviewCodingPractice/src/Practice/PrintOccuranceOfWordsInGivenString.java
@@ -1,11 +1,32 @@
 package Practice;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class PrintOccuranceOfWordsInGivenString 
 {
 
 	public static void main(String[] args) 
 	{
+		String str="   I Love Love Coding and and and Learning Learning Learning  ";
+		Map<String, Integer> map=new LinkedHashMap<String, Integer>();
+		Integer count=1;
 		
+		String[] word=str.toLowerCase().trim().split(" ");
+		for(int i=0;i<word.length;i++)
+		{
+			if(!map.containsKey(word[i]))
+				map.put(word[i], count);	
+			else
+				map.put(word[i],map.get(word[i])+1);
+			
+		}
+		
+		for(String w:map.keySet())
+		{
+			System.out.println("The count of word: "+w+"= "+map.get(w));
+		}
 
 	}
 
