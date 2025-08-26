5325947d101f27d19a60465db11b7d422fb3fce0
ashamit <ashwiniamit13@gmail.com>
2025-08-26 21:14:54 +0530

    Program to print first non-repeated character in given string

---
+++ InterviewCodingPractice/src/Practice/FirstNonRepeatedCharacter.java
@@ -0,0 +1,37 @@
+package Practice;
+
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.LinkedHashMap;
+
+public class FirstNonRepeatedCharacter {
+
+	public static void main(String[] args) 
+	{
+		//String str="swiss"; //only w is the only first non repeated character
+		String str="programming";
+		
+		Map<Character,Integer> map=new LinkedHashMap<Character,Integer>();
+		Integer count=1;
+		
+		for(int i=0;i<str.length();i++)
+		{
+			if(!map.containsKey(str.charAt(i)))
+				map.put(str.charAt(i), count);
+			else
+				map.put(str.charAt(i), map.get(str.charAt(i))+1);
+		}
+		
+		for(Entry<Character,Integer> entry:map.entrySet())
+		{
+			if(entry.getValue()==1)
+			{
+				System.out.println("First repeated character is : "+entry.getKey());
+				break;
+			}		
+			
+		}
+
+	}
+
+}
